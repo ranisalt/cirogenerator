13821850227fdc868c5d45286e86d3502aebfe44
Ranieri Althoff <ranisalt@gmail.com>
2018-09-25 21:27:44 -0300

    Add initial files

---
+++ index.js
@@ -0,0 +1,73 @@
+(() => {
+  const parts = [
+    [
+      'bandidinho',
+      'cadela no cio',
+      'chefe de quadrilha',
+      'conspirador',
+      'golpista',
+      'jumento de carga',
+      'maior bandido',
+      'na sua vaidade de',
+      'playboy',
+      'projetinho de',
+      'protoneoliberalzinho',
+      'salafrário',
+    ], [
+      'abestado',
+      'abirobado',
+      'amalucado',
+      'apartidário',
+      'bobão',
+      'boçal',
+      'chefe de milícia',
+      'cheirador de cocaína',
+      'covarde',
+      'delinquente juvenil',
+      'despreparado',
+      'farsante',
+      'fascistoide',
+      'filho da puta',
+      'fisiológico',
+      'frouxo',
+      'fuxiqueiro',
+      'idiota',
+      'imbecil',
+      'infeliz',
+      'liberaloide',
+      'marginal',
+      'mentiroso',
+      'oportunista',
+      'picareta',
+      'protofascistinha',
+      'quadrilheiro',
+      'safado',
+      'selvagem',
+      'vil e pulsânime',
+    ], [
+      'cheio de areia no cu',
+      'de merda',
+      'desgraçado',
+      'dos grandes',
+      'dos infernos',
+      'eleitoreiro',
+    ],
+  ]
+
+  document.addEventListener('DOMContentLoaded', ev => {
+    const button = document.querySelector('.button'),
+      output = document.querySelector('.output')
+
+    const generateNew = () => {
+      output.innerText = parts.map(
+        part => part[Math.floor(Math.random() * part.length)]
+      ).join(' ')
+    }
+
+    button.addEventListener('click', ev => {
+      window.requestAnimationFrame(generateNew)
+    })
+
+    window.requestAnimationFrame(generateNew)
+  })
+})()
